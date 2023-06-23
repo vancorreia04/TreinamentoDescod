9b5325cf1c276b5ada732305f8b8cefd64018ea3
vancorreia04 <109099486+vancorreia04@users.noreply.github.com>
2023-06-23 18:55:27 -0300

    Add files via upload

---
+++ Funções/script.js
@@ -0,0 +1,5 @@
+function medianotas() {
+    var x = Number(document.getElementById("N1").value);
+    var y = Number(document.getElementById("N2").value);
+    document.getElementById("resultado").innerHTML = (x + y) / 2;
+    }
